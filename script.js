ff7f8e38dd1cf2e2dea557d56499e7d4ac943154
tassirof <mohammad.sharafi77@gmail.com>
2022-09-03 22:13:52 +0430

    step 22

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.querySelector("iframe").cookie =
-    cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+document.cookie = "SameSite=None; Secure";
